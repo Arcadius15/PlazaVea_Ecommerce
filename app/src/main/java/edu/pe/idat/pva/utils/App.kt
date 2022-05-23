71ba326766cba798283d00edfec143b46f0908c5
Diego Vicente <diegovic99@hotmail.com>
2022-05-22 22:54:23 -0500

    primer commit

---
+++ app/src/main/java/edu/pe/idat/pva/utils/App.kt
@@ -0,0 +1,15 @@
+package edu.pe.idat.pva.utils
+
+import android.app.Application
+
+class App: Application() {
+    companion object{
+        lateinit var instance: App
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+}

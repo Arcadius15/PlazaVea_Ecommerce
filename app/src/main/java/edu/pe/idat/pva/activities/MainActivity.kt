bb78c0e6361d4b5bb3bd5ba28407665bdd77a0a8
Andrea Sotil <andreasotil@ieholding.com>
2022-06-03 16:33:53 -0500

    login y registro

--- app/src/main/java/edu/pe/idat/pva/activities/MainActivity.kt
+++ app/src/main/java/edu/pe/idat/pva/activities/MainActivity.kt
@@ -3,25 +3,43 @@ package edu.pe.idat.pva.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.View
+import android.widget.*
 import edu.pe.idat.pva.R
 import edu.pe.idat.pva.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     var imageViewGoToRegister: ImageView? = null
+    var editTextEmail: EditText? = null
+    var editTextPassword: EditText? = null
+    var buttonLogin: Button? = null
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        imageViewGoToRegister = findViewById(R.id.imageview_go_register)
+        imageViewGoToRegister = findViewById(R.id.btn_go_register)
+        editTextEmail = findViewById(R.id.edTextEmail)
+        editTextPassword = findViewById(R.id.edTextPassword)
+        buttonLogin = findViewById(R.id.btn_login)
+
 
         imageViewGoToRegister?.setOnClickListener{ goToRegister() }
+        buttonLogin?.setOnClickListener{login()}
+    }
+
+    private fun login(){
+        val email = editTextEmail?.text.toString()
+        val password = editTextPassword?.text.toString()
+
+        Toast.makeText(this, "El email es: $email", Toast.LENGTH_LONG).show()
     }
 
     private fun goToRegister(){
         val i = Intent(this, RegisterActivity::class.java)
         startActivity(i)
     }
+
+
 }
--- app/src/main/java/edu/pe/idat/pva/activities/RegisterActivity.kt
+++ app/src/main/java/edu/pe/idat/pva/activities/RegisterActivity.kt
@@ -3,21 +3,38 @@ package edu.pe.idat.pva.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
+import android.widget.Toast
 import edu.pe.idat.pva.R
+import edu.pe.idat.pva.databinding.ActivityRegisterBinding
 
-class RegisterActivity : AppCompatActivity() {
-    var imageViewGoToLogin: ImageView? = null
+class RegisterActivity : AppCompatActivity() , View.OnClickListener {
+
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
-        imageViewGoToLogin = findViewById(R.id.imageview_go_to_login)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        imageViewGoToLogin?.setOnClickListener{ goToLogin() }
+        binding.imageviewGoToLogin.setOnClickListener{toast()}
+        binding.btnRegistrar.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View) {
+
+
     }
 
     private fun goToLogin(){
         val i = Intent(this, MainActivity::class.java)
         startActivity(i)
     }
+
+    private fun toast(){
+        Toast.makeText(this, "hizo click", Toast.LENGTH_LONG).show()
+    }
+
+
+
 }
